a25ba450b5942045b50b4922e7c020d3fe19e6bb
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 11:10:40 -0500

    Change style of input form

--- src/components/NavBar/InputForm/InputForm.jsx
+++ src/components/NavBar/InputForm/InputForm.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const InputForm = () => {
     return (
-        <form className="container mx-auto flex flex-col w-2/3">
-            <label>Name: </label>
-            <input type="text" name="name" className="rounded px-2 py-1.5"/>
-            <label>Ingredients: </label>
-            <input type="text" name="ingredient" className="rounded px-2 py-1.5"/>
-            <label>Directions: </label>
-            <textarea name="directions" className="rounded px-2 py-1.5 resize-none" />
-            <label>Notes: </label>
-            <textarea name="notes" className="rounded px-2 py-1.5 resize-none" />
-        </form>
+        <div className="py-5 bg-emerald-700">
+            <form className="container mx-auto flex flex-col w-2/3">
+                <label>Name: </label>
+                <input type="text" name="name" className="rounded px-2 py-1.5"/>
+                <label>Ingredients: </label>
+                <input type="text" name="ingredient" className="rounded px-2 py-1.5"/>
+                <label>Directions: </label>
+                <textarea name="directions" className="rounded px-2 py-1.5 resize-none" />
+                <label>Notes: </label>
+                <textarea name="notes" className="rounded px-2 py-1.5 resize-none" />
+            </form>
+        </div>
     )
 }
 
