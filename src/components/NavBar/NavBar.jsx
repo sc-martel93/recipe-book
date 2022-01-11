faaf0f79df06d7b3fcce575e0ae83db72fdf16d2
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 11:22:07 -0500

    Add functionality to create button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch} from '@fortawesome/free-solid-svg-icons'
 import InputForm from './InputForm/InputForm'
 
 const NavBar = () => {
+    const [isCreating, setIsCreating] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
 
+
     return (
         <div className="h-16 bg-emerald-900 ">
             <nav className="h-full container mx-auto max-w-6xl flex justify-around items-center">
                 <h1 className="text-slate-200 text-4xl">Recipes</h1>
-                <button 
+                <button
+                    onClick={() => setIsCreating(!isCreating)}
                     className="bg-slate-300 hover:bg-cyan-600 hover:outline  transition-colors font-bold rounded px-5 py-1.5"
                 >
-                    Create
+                    {isCreating ? "Close" : "Creating"}
                 </button>
                 <form onSubmit={e => handleSubmit(e)} className="">
                     <input type="text" className="rounded px-2 py-1.5" />
@@ -28,7 +31,8 @@ const NavBar = () => {
                     </button>
                 </form>
             </nav>
-            <InputForm />
+            {isCreating ? <InputForm /> : null}
+            
         </div>
     )
 }
