e9f720662bab7c62d5b02035beeed3c712c43065
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 19:05:02 -0500

    Add navigations buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useSelector } from "react-redux";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 import NavBar from "./components/NavBar/NavBar";
 import Recipe from "./components/Recipe/Recipe";
 
@@ -9,6 +12,16 @@ function App() {
   return (
     <div className="h-screen bg-emerald-500">
       <NavBar />
+
+      <div className="flex justify-around max-w-2/3 mt-5">
+        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+
       <Recipe
         recipe={
           recipes[0] || { name: "", ingredients: "", directions: "", notes: "" }
