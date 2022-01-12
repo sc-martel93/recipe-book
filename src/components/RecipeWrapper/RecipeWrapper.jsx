0b8e42c5ae499a508181d5af1a13fdfcfaa546fb
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 20:02:42 -0500

    Add page label for page count in RecipeWrapper.jsx

--- src/components/RecipeWrapper/RecipeWrapper.jsx
+++ src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -30,7 +30,7 @@ const RecipeWrapper = () => {
         >
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
-        <h3>{recipeIndex + 1}</h3>
+        <h3>Page {recipeIndex + 1}</h3>
         <button
           onClick={() => next()}
           className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5"
