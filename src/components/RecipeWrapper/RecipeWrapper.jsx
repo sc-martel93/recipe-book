70082118ed99bd6678d2e9b82259290f5f3cff45
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-12 16:12:14 -0500

    Move recipeIndex state to redux

--- src/components/RecipeWrapper/RecipeWrapper.jsx
+++ src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { next, previous, setIndex } from "../../state/actions/recipeIndex";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
 import Recipe from "./Recipe/Recipe";
 
 const RecipeWrapper = () => {
-  let [recipeIndex, setRecipeIndex] = useState(0);
+  const dispatch = useDispatch();
+  const recipeIndex = useSelector((state) => state.recipeIndex);
   const recipes = useSelector((state) => state.recipes);
   const recipeCount = recipes.length;
 
-  const previous = () => {
+  const handlePrevious = () => {
     if (recipeCount == 0) return;
-    if (recipeIndex <= 0) return setRecipeIndex(recipeCount - 1);
-    setRecipeIndex(recipeIndex - 1);
+    if (recipeIndex <= 0) return dispatch(setIndex(recipeCount - 1));
+    dispatch(previous());
   };
 
-  const next = () => {
-    if (recipeIndex >= recipeCount - 1) return setRecipeIndex(0);
-    setRecipeIndex(recipeIndex + 1);
+  const handleNext = () => {
+    if (recipeIndex >= recipeCount - 1) return dispatch(setIndex(0));
+    dispatch(next());
   };
 
   return (
     <>
       <div className="flex justify-around items-center font-bold max-w-2/3 mt-5">
         <button
-          onClick={() => previous()}
+          onClick={() => handlePrevious()}
           className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5"
         >
           <FontAwesomeIcon icon={faArrowLeft} />
@@ -34,7 +36,7 @@ const RecipeWrapper = () => {
           Page {recipeIndex + 1} out of {recipeCount}
         </h3>
         <button
-          onClick={() => next()}
+          onClick={() => handleNext()}
           className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5"
         >
           <FontAwesomeIcon icon={faArrowRight} />
--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { recipeIndexReducer } from "./recipeIndexReducer";
 import { recipeReducer } from "./recipeReducer";
 
 export default combineReducers({
   recipes: recipeReducer,
+  recipeIndex: recipeIndexReducer,
 });
