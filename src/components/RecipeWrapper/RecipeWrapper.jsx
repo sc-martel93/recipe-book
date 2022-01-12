2bb4ee744e446d88fa292b33ec0fdb6bcb6e9c8e
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 19:31:16 -0500

    Add index state for RecipeWrapper navigation buttons

--- src/components/RecipeWrapper/RecipeWrapper.jsx
+++ src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
@@ -6,21 +6,34 @@ import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Recipe from "./Recipe/Recipe";
 
 const RecipeWrapper = () => {
+  let [recipeIndex, setRecipeIndex] = useState(0);
   const recipes = useSelector((state) => state.recipes);
 
+  const handlePrevious = () => {
+    setRecipeIndex(recipeIndex - 1);
+  };
+
+  const handleNext = () => {
+    setRecipeIndex(recipeIndex + 1);
+  };
+
   return (
-    <>
-      <div className="flex justify-around max-w-2/3 mt-5">
-        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-        <h3>{}</h3>
-        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
-    </>
+    <div className="flex justify-around items-center font-bold max-w-2/3 mt-5">
+      <button
+        onClick={() => handlePrevious()}
+        className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5"
+      >
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
+      <h3>{recipeIndex}</h3>
+      <button
+        onClick={() => handleNext()}
+        className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5"
+      >
+        <FontAwesomeIcon icon={faArrowRight} />
+      </button>
+    </div>
   );
 };
 
-export default Recipe;
+export default RecipeWrapper;
