706c25f9910df6d140b3b0bc93d1013191fc7ae2
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-12 16:37:53 -0500

    Code cleanup

--- src/components/RecipeWrapper/RecipeWrapper.jsx
+++ src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { next, previous, setIndex } from "../../state/actions/recipeIndex";
 
