f4b229cb69ea82224793267c8ae72570c00057df
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-12 10:25:22 -0500

    Style for Recipe.jsx

--- src/components/RecipeWrapper/Recipe/Recipe.jsx
+++ src/components/RecipeWrapper/Recipe/Recipe.jsx
@@ -2,13 +2,22 @@ import React from "react";
 
 const Recipe = ({ recipe }) => {
   const { name, ingredients, directions, notes } = recipe;
+  const ingredientArray = ingredients.split(", ");
+
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{ingredients}</p>
+    <article className="w-screen flex flex-col items-center space-y-5 ">
+      <h2 className="text-3xl font-bold">{name}</h2>
+
+      <ol className="list-disc">
+        <p className="font-bold mb-2">Ingredients</p>
+        {ingredientArray.map((ingredient, index) => {
+          return <li key={index}>{ingredient}</li>;
+        })}
+      </ol>
+      <p>Directions</p>
       <p>{directions}</p>
       <p>{notes}</p>
-    </div>
+    </article>
   );
 };
 
