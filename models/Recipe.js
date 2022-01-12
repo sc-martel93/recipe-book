e28b8fab3b90e0f4cb89d284fcc23c29d66da291
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-12 17:37:28 -0500

    Set up recipe class

---
+++ models/Recipe.js
@@ -0,0 +1,38 @@
+import db = require("../config/db");
+
+class Recipe {
+  constructor(recipe) {
+    const { id, name, ingredients, directions, notes } = recipe;
+    this.id = id;
+    this.name = name;
+    this.ingredients = ingredients;
+    this.directions = directions;
+    this.notes = notes;
+
+    return this;
+  }
+
+  save() {
+    const sql = `INSERT INTO recipes(
+        id,
+        name,
+        ingredients,
+        directions,
+        notes
+     )
+     VALUE (
+        '${this.id}',
+        '${this.name}',
+        '${this.ingredients}',
+        '${this.directions}',
+        '${this.notes}'
+     )`;
+
+    return db.execute(sql);
+  }
+
+  static findAll = () => {
+    const sql = `SELECT * FROM recipes ORDER BY name`;
+    return db.execute(sql)
+  };
+}
