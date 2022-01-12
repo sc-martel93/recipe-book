e674b209ca1d290aefb5d39406d40c01c063ac34
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-11 19:08:33 -0500

    Move recipes data from App.js to Recipe.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { useSelector } from "react-redux";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
 import NavBar from "./components/NavBar/NavBar";
 import Recipe from "./components/Recipe/Recipe";
 
 function App() {
-  const recipes = useSelector((state) => state.recipes);
-
   return (
     <div className="h-screen bg-emerald-500">
       <NavBar />
-
-      <div className="flex justify-around max-w-2/3 mt-5">
-        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
-
-      <Recipe
-        recipe={
-          recipes[0] || { name: "", ingredients: "", directions: "", notes: "" }
-        }
-      />
+      <Recipe />
     </div>
   );
 }
--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const Recipe = ({ recipe }) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+const Recipe = () => {
+  const recipes = useSelector((state) => state.recipes);
   return (
-    <div className="">
-      <h2>{recipe.name}</h2>
-      <p>{recipe.ingredients}</p>
-      <p>{recipe.directions}</p>
-      <p>{recipe.notes}</p>
-    </div>
+    <>
+      <div className="flex justify-around max-w-2/3 mt-5">
+        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button className="bg-slate-300 hover:bg-cyan-600 hover:outline transition-colors rounded px-5 py-1.5">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+
+      <h2>{}</h2>
+      <p>{}</p>
+      <p>{}</p>
+      <p>{}</p>
+    </>
   );
 };
 
