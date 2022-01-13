085da67c3b3a3b81ab6d578dcb668af9a3143b11
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 09:14:17 -0500

    Set up load recipes from database onMount

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getRecipes } from "./state/actions/recipes";
+
 import NavBar from "./components/NavBar/NavBar";
 import RecipeWrapper from "./components/RecipeWrapper/RecipeWrapper";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRecipes());
+  }, [dispatch]);
+
   return (
     <div className="h-100 min-h-screen bg-emerald-500">
       <NavBar />
