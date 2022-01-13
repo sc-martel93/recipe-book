a4e0b1be1dd9bb7d45d5c93a390139faaad58cea
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 09:24:19 -0500

    Add createRecipe function to api

--- src/state/api/index.js
+++ src/state/api/index.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const URL = "http://localhost:3001/recipes";
 
 export const fetchRecipes = () => axios.get(URL);
+export const createRecipe = (newRecipe) => axios.post(URL, newRecipe);
