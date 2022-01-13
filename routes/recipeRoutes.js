ffe89fb30c80d52dfd8a434f1293ca63fdb712ab
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 08:42:19 -0500

    Set up get request for /recipes route

---
+++ routes/recipeRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const recipeControllers = require("../controllers/recipeControllers");
+const router = express.Router();
+
+router.route("/").get(recipeControllers.getAllRecipes);
