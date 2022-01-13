29d344c054009d4ecbb76c4c1be73f9cfc2ac59f
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 08:30:02 -0500

    Cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-const PORT = process.env.port || 3001;
-
-app.listen(PORT, (req, res) => {
-  console.log(`Listening on port ${PORT}`);
-});
