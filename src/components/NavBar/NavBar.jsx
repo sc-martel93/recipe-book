36a0e43970c72664b691c3f4a6e95c208e8f9dc0
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 11:34:57 -0500

    Change NavBar spacing in mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
 
   return (
     <div className="bg-emerald-900">
-      <nav className="max-w-6xl mx-auto flex flex-col md:flex-row lg:flex-row justify-between md:justify-evenly lg:justify-evenly items-center h-36 md:h-16 lg:h-16">
+      <nav className="max-w-6xl mx-auto flex flex-col md:flex-row lg:flex-row justify-evenly items-center h-44 md:h-16 lg:h-16">
         <h1 className="text-slate-200 text-4xl">Recipes</h1>
 
         <button
