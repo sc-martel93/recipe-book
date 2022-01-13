2794ab3d6a888210e01514922f4f3977b1d97abf
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-13 08:44:41 -0500

    Add router module export

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -3,3 +3,5 @@ const recipeControllers = require("../controllers/recipeControllers");
 const router = express.Router();
 
 router.route("/").get(recipeControllers.getAllRecipes);
+
+module.exports = router;
