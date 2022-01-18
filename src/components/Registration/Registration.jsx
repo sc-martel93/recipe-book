52973a3a6b0c28e35ceb3903782cf4561a1c2448
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-18 15:46:17 -0500

    Registration.js cleanup

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -4,16 +4,35 @@ const Registration = () => {
   return (
     <div>
       <form className="mx-auto flex flex-col items-center w-2/3 max-w-3xl">
-        <div className="mt-2">
+        <div className="mt-2 flex justify-evenly">
           <label>Username</label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            className="w-3/5 rounded px-2 py-1.5"
+            required
+          />
         </div>
-        <div className="mt-2">
-          <label>Password</label>
-          <input type="password" name="password" />
+        <div className="mt-2 flex justify-evenly">
+          <label>Password </label>
+          <input
+            type="password"
+            name="password"
+            className="w-3/5 rounded px-2 py-1.5"
+            required
+          />
+        </div>
+        <div className="mt-2 flex justify-evenly">
+          <label>Password </label>
+          <input
+            type="password"
+            name="password"
+            className="w-3/5 rounded px-2 py-1.5"
+            required
+          />
         </div>
         <button className="outline-none bg-slate-300 hover:bg-cyan-400 focus:bg-cyan-400 transition-colors font-bold rounded px-5 py-1.5 my-3">
-          Submit
+          Register
         </button>
       </form>
     </div>
