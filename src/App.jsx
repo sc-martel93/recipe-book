34827d260772d666041c5fdf56498570b33fdf03
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-20 15:58:43 -0500

    Create RecipePage.jsx and reformat routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import NavBar from "./components/NavBar/NavBar";
-import RecipeWrapper from "./components/RecipeWrapper/RecipeWrapper";
-import Registration from "./pages/Registration/Registration";
+import RecipePage from "./pages/Recipes/RecipePage";
+import RegistrationPage from "./pages/Registration/RegistrationPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="h-100 min-h-screen bg-emerald-500 font-poppins">
-        <NavBar />
         <Routes>
-          <Route path="/register" element={<Registration />} />
-          <Route path="recipes" element={<RecipeWrapper />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="recipes" element={<RecipePage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/Recipes/RecipePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+import RecipeWrapper from "../../components/RecipeWrapper/RecipeWrapper";
+
+const RecipePage = () => {
+  return (
+    <>
+      <NavBar />
+      <RecipeWrapper />
+    </>
+  );
+};
+
+export default RecipePage;
