68fa76bf39c01e896018340a177a708d10a13b33
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-25 13:28:11 -0500

    Add images to HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import cake from "../../assets/images/cake.jpg";
+import spaghetti from "../../assets/images/spaghetti.jpg";
+import vegetables from "../../assets/images/vegetables.jpg";
+import salmon from "../../assets/images/salmon.jpg";
+
 const HomePage = () => {
   return (
     <main>
@@ -19,7 +24,15 @@ const HomePage = () => {
           </p>
         </div>
       </section>
-      <section className="flex justify-around max-w-6xl mx-auto py-20">
+
+      <section className="flex justify-evenly max-w-5xl mx-auto space-x-5">
+        <img src={cake} alt="cake" className="w-1/4 max-w-3xl" />
+        <img src={vegetables} alt="vegetables" className="w-1/4 max-w-3xl" />
+        <img src={spaghetti} alt="spaghetti" className="w-1/4 max-w-3xl" />
+        <img src={salmon} alt="salmon" className="w-1/4 max-w-3xl" />
+      </section>
+
+      <section className="flex justify-around max-w-6xl mx-auto py-20 shadow-lg shadow-emerald-800">
         <Link
           to="/login"
           className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 my-3 mx-auto"
@@ -39,6 +52,7 @@ const HomePage = () => {
           Explore
         </Link>
       </section>
+
       <footer className="h-60 absolute inset-x-0 bottom-0 bg-emerald-700"></footer>
     </main>
   );
