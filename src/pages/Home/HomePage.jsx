3da69555b4ca8cb096dec8fd1f38e9cf4eda5115
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-25 13:45:51 -0500

    refactor header on HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -9,7 +9,7 @@ import salmon from "../../assets/images/salmon.jpg";
 const HomePage = () => {
   return (
     <main>
-      <section className="bg-emerald-700 shadow-lg shadow-emerald-800 h-72 flex items-center justify-center flex-col">
+      <header className="bg-emerald-700 shadow-lg shadow-emerald-800 h-72 flex items-center justify-center flex-col space-y-8">
         <h1 className="text-5xl md:text-6xl lg:text-6xl">
           Recipe <span className="font-semibold text-orange-400">Book</span>
         </h1>
@@ -23,34 +23,34 @@ const HomePage = () => {
             recipes
           </p>
         </div>
-      </section>
+
+        <nav className="flex justify-around mx-auto space-x-5">
+          <Link
+            to="/login"
+            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
+          >
+            Register
+          </Link>
+          <Link
+            to="/recipes"
+            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
+          >
+            Explore
+          </Link>
+        </nav>
+      </header>
 
       <section className="flex justify-evenly max-w-5xl mx-auto space-x-5">
-        <img src={cake} alt="cake" className="w-1/4 max-w-3xl" />
+        <img src={cake} alt="cake" className="w-1/4 min-w-3xl" />
         <img src={vegetables} alt="vegetables" className="w-1/4 max-w-3xl" />
         <img src={spaghetti} alt="spaghetti" className="w-1/4 max-w-3xl" />
         <img src={salmon} alt="salmon" className="w-1/4 max-w-3xl" />
-      </section>
-
-      <section className="flex justify-around max-w-6xl mx-auto py-20 shadow-lg shadow-emerald-800">
-        <Link
-          to="/login"
-          className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 my-3 mx-auto"
-        >
-          Login
-        </Link>
-        <Link
-          to="/register"
-          className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 my-3 mx-auto"
-        >
-          Register
-        </Link>
-        <Link
-          to="/recipes"
-          className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 my-3 mx-auto"
-        >
-          Explore
-        </Link>
       </section>
 
       <footer className="h-60 absolute inset-x-0 bottom-0 bg-emerald-700"></footer>
