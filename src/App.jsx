6e61157fb310ece1fe932dd57a3e963eea7372d4
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-01-25 09:44:01 -0500

    Remove excessive div tags

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import RegistrationPage from "./pages/Registration/RegistrationPage";
 function App() {
   return (
     <BrowserRouter>
-      <div className="h-100 min-h-screen bg-emerald-500 font-poppins">
+      <main className="h-100 min-h-screen bg-emerald-500 font-poppins">
         <Routes>
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="recipes" element={<RecipePage />} />
         </Routes>
-      </div>
+      </main>
     </BrowserRouter>
   );
 }
