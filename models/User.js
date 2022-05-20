54682fd12c5ff5fc0bd7cc4f05d163305d7f76c9
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-20 17:18:52 -0400

    Remove email field from user

--- models/User.js
+++ models/User.js
@@ -3,20 +3,20 @@ const uuid = require("uuid");
 const bcrypt = require("bcrypt");
 
 class User {
-  constructor({ name, password, email }) {
+  constructor({ name, password }) {
     this.id = uuid.v4();
     this.name = name;
     this.password = password;
-    this.email = email;
   }
 
   async save() {
     const sql =
-      "INSERT INTO users (id, name, password, email) VALUES (?, ?, ?, ?)";
+      "INSERT INTO users (id, name, password) VALUES (?, ?, ?)";
+      // db.execute(sql, [this.id, this.name, this.password]);
 
     const hash = await hashPassword(this.password);
     return db
-      .execute(sql, [this.id, this.name, hash, this.email])
+      .execute(sql, [this.id, this.name, hash])
       .catch((err) => {
         return err.code;
       });
