7bcb9087055ea27a9fd96dea47a6b3623132de85
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-21 18:31:36 -0400

    Add status to respone for user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,24 +7,23 @@ exports.createUser = async (req, res, next) => {
     const result = await user.save();
 
     if (result === "ER_DUP_ENTRY") {
-      return res.status(409).json({ message: "User name already exists" });
+      return res.status(409).json({ status: "error", message: "User name already exists" });
     }
-
-    res
-      .status(201)
-      .json({ message: "User Created Successfully", name: user.name });
+    return res.status(201).json({ status: "ok", message: "User Created Successfully", name: user.name });
   } catch (error) {
     console.log(error);
     next(error);
+    return res.status(503).json({ status: "error", message: "Service error" });
   }
 };
 
 exports.getUserId = async (req, res, next) => {
   try {
     let [userId, _] = await User.findUserId(req.params.name);
-    res.status(200).json({message: "User found", userId});
+    res.status(200).json({ status: "ok", message: "User found", userId});
   } catch (error) {
     console.log(error);
     next(error);
+    return res.status(404).json({ status: "error", message: "User name not found" });
   }
 }
