5ecf9252ea62cdbc6fb4e4e8ddb9636991eff1b7
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-23 11:06:17 -0400

    Remove unused function, rerout user/register

--- src/state/api/index.js
+++ src/state/api/index.js
@@ -6,5 +6,5 @@ export const fetchRecipes = () => axios.get(`${URL}/recipes`);
 export const createRecipe = (newRecipe) => axios.post(`${URL}/recipes`, newRecipe);
 export const deleteRecipe = (id) => axios.delete(`${URL}/recipes/${id}`);
 
-export const createUser = (newUser) => axios.post(`${URL}/register`, newUser);
-export const fetchUserId = (name) => axios.get(`${URL}/register/${name}`)
+export const createUser = (newUser) => axios.post(`${URL}/user/register`, newUser);
+
