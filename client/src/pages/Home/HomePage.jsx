1d4e8aefa7b9ebb474e4e7994358faa46c1db10b
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-26 14:35:54 -0400

    Change home page

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import cake from "../../assets/images/cake.jpg";
-import spaghetti from "../../assets/images/spaghetti.jpg";
-import vegetables from "../../assets/images/vegetables.jpg";
-import salmon from "../../assets/images/salmon.jpg";
+import { useState } from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 
 const HomePage = () => {
+  const [login, setLogin] = useState(false);
+  const [register, setRegister] = useState(false);
+
   return (
     <main>
       <header className="bg-emerald-700 shadow-lg shadow-emerald-800 h-72 flex items-center justify-center flex-col space-y-8">
@@ -25,42 +24,26 @@ const HomePage = () => {
         </section>
 
         <nav className="flex justify-around mx-auto space-x-5">
-          <Link
-            to="/login"
-            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
+          <button
+            onClick={() => setLogin(login => !login)}
+            className="outline-none bg-slate-300 hover:bg-orange-400 
+                    focus:bg-orange-400 transition-colors font-bold 
+                      rounded px-5 py-1.5 mx-auto"
           >
             Login
-          </Link>
-          <Link
-            to="/register"
-            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
+          </button>
+          <button
+            onClick={() => setRegister(register => !register)}
+            className="outline-none bg-slate-300 hover:bg-orange-400 
+                    focus:bg-orange-400 transition-colors font-bold 
+                      rounded px-5 py-1.5 mx-auto"
           >
             Register
-          </Link>
-          <Link
-            to="/recipes"
-            className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors font-bold rounded px-5 py-1.5 mx-auto"
-          >
-            Explore
-          </Link>
+          </button>
         </nav>
       </header>
-
-      <section className="max-w-6xl space-y-5 mx-auto">
-        <h3 className="text-xl text-center font-bold mt-5">Desserts!</h3>
-
-        <img src={cake} alt="cake" className="w-5/6 mx-auto" />
-        <h3 className="text-xl text-center font-bold">Pasta!</h3>
-        <img src={spaghetti} alt="spaghetti" className="w-5/6 mx-auto" />
-
-        <h3 className="text-xl text-center font-bold">Vegetarian!</h3>
-        <img src={vegetables} alt="vegetables" className="w-5/6 mx-auto" />
-
-        <h3 className="text-xl text-center font-bold">Seafood!</h3>
-        <img src={salmon} alt="salmon" className="w-5/6 mx-auto" />
-      </section>
-
-      {/* <footer className="h-60 absolute inset-x-0 bottom-0 bg-emerald-700"></footer> */}
+      { register ? <RegistrationForm /> : null }
+      { login ? <LoginForm /> : null }
     </main>
   );
 };
