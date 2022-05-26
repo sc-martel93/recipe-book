f6a8f943425a0bc7c1dd6898f279b715d4243027
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-26 18:52:16 -0400

    Remove unused pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
-import LoginPage from "./pages/Login/LoginPage";
-
 import RecipePage from "./pages/Recipes/RecipePage";
-import RegistrationPage from "./pages/Registration/RegistrationPage";
+
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <main className="h-100 min-h-screen bg-blue-300 font-poppins">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="register" element={<RegistrationPage />} />
-          <Route path="login" element={<LoginPage />} />
           <Route path="recipes" element={<RecipePage />} />
         </Routes>
       </main>
