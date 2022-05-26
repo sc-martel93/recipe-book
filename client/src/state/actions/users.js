36a2264de1b5ce38ed16d99b7fd4f2082f9d23d8
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-26 11:43:54 -0400

    Code clean up

--- client/src/state/actions/users.js
+++ client/src/state/actions/users.js
@@ -3,11 +3,6 @@ import * as api from "../api";
 export const createUser = (newUser) => async (dispatch) => {
     try {
         const results = await api.createUser(newUser);
-
-        // if (res.data.status === "ok"){
-        //   return res.data.status;
-        // }
-
         return results;
 
       } catch (error) {
@@ -19,6 +14,7 @@ export const loginUser = (userInfo) => async (dispatch) => {
   try {
     const results = await api.loginUser(userInfo);
     return results;
+    
   } catch (error) {
     return error;
   }
