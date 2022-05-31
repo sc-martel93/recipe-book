bd0264f15276408acf4336ebe06baa28c0727f19
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-31 10:27:12 -0400

    Cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import RecipePage from "./pages/Recipes/RecipePage";
