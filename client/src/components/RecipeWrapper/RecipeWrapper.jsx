0db750db227bbc6f40217d7c237649f9c94876c2
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-31 13:00:04 -0400

    Update recipe wrapper buttons

--- client/src/components/RecipeWrapper/RecipeWrapper.jsx
+++ client/src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -34,7 +34,7 @@ const RecipeWrapper = () => {
         <button
           onClick={() => handlePrevious()}
           title="Previous"
-          className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors rounded px-5 py-1.5"
+          className="text-white outline-none bg-blue-900 hover:bg-yellow-400 focus:bg-yellow-400 hover:text-blue-900 focus:text-blue-900 transition-colors rounded px-5 py-1.5"
         >
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
@@ -44,7 +44,7 @@ const RecipeWrapper = () => {
         <button
           onClick={() => handleNext()}
           title="Next"
-          className="outline-none bg-slate-300 hover:bg-orange-400 focus:bg-orange-400 transition-colors rounded px-5 py-1.5"
+          className="text-white outline-none bg-blue-900 hover:bg-yellow-400 focus:bg-yellow-400 hover:text-blue-900 focus:text-blue-900 transition-colors rounded px-5 py-1.5"
         >
           <FontAwesomeIcon icon={faArrowRight} />
         </button>
