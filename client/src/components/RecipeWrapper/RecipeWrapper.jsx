f664225d95d09d4d64cf9236286f8ed22b60977b
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-31 14:20:50 -0400

    Implement drop down menu for recipe wrapper

--- client/src/components/RecipeWrapper/RecipeWrapper.jsx
+++ client/src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Dropdown from 'react-dropdown';
 import { next, previous, setIndex } from "../../state/actions/recipeIndex";
 import { getAllRecipes } from "../../state/actions/recipes";
 
@@ -13,12 +14,20 @@ const RecipeWrapper = () => {
   const recipes = useSelector((state) => state.recipes);
   const recipeCount = recipes.length;
 
+  const options = [
+    {value: 1, label: "All Recipes"},
+    {value: 2, label: "My Recipes"},
+    {value: 3, label: "Liked Recipes"}
+  ];
+  const defaultOption = options[0];
+
+
   useEffect(() => {
     dispatch(getAllRecipes());
   }, [dispatch]);
 
   const handlePrevious = () => {
-    if (recipeCount == 0) return;
+    if (recipeCount === 0) return;
     if (recipeIndex <= 0) return dispatch(setIndex(recipeCount - 1));
     dispatch(previous());
   };
@@ -39,7 +48,7 @@ const RecipeWrapper = () => {
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
         <h3>
-          Page {recipeCount == 0 ? 0 : recipeIndex + 1} out of {recipeCount}
+          Page {recipeCount === 0 ? 0 : recipeIndex + 1} out of {recipeCount}
         </h3>
         <button
           onClick={() => handleNext()}
@@ -50,7 +59,16 @@ const RecipeWrapper = () => {
         </button>
       </section>
 
-
+      <div className="flex justify-evenly text-center max-w-4xl mx-auto mt-10">
+        <Dropdown 
+          options={options} 
+          onChange={e => console.log(e)}
+          value={defaultOption} 
+          className="w-72"
+          placeholderClassName="cursor-pointer text-white bg-blue-900 hover:bg-yellow-400 focus:bg-yellow-400 hover:text-blue-900 focus:text-blue-900 transition-colors rounded px-5 py-1.5"
+          menuClassName="bg-blue-500 p-5 mt-1 rounded-lg shadow-lg shadow-slate-600"
+        />
+      </div>
 
       {recipeCount === 0 ? (
         <h2 className="font-bold text-center text-3xl mt-20">No Recipes</h2>
