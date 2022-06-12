e45f20703babfd60b66f8d1da932e06a5f24fb19
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-12 13:05:43 -0400

    Fix login button size on nav bar

--- client/src/components/HomeNav/HomeNav.jsx
+++ client/src/components/HomeNav/HomeNav.jsx
@@ -54,7 +54,7 @@ const HomeNav = () => {
         :
           <button
             onClick={() => navigate("/")}
-            className="outline-none bg-slate-300 hover:bg-yellow-400 focus:bg-yellow-400 w-56 md:w-fit lg:w-fit transition-colors font-bold rounded px-5 py-1.5 md:ml-20"
+            className="outline-none bg-slate-300 hover:bg-yellow-400 focus:bg-yellow-400 w-56 md:w-44 lg:w-44 transition-colors font-bold rounded px-5 py-1.5 md:ml-20"
           >
             Login
           </button>
