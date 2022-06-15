20071eeb957832f1b9b722ca89ad3359f8fa4867
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-15 09:27:43 -0400

    Add check for logged in user on load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import RecipePage from "./pages/Recipes/RecipePage";
-
+import { setUser } from './state/actions/users'
 
 function App() {
+  const dispatch = useDispatch();
+  
+  // Checks if a user is saved on local storage and load their data
+  useEffect(() => {
+    let currentUser = JSON.parse(window.localStorage.getItem("RECIPE_USER"));
+  
+    if(currentUser?.isLoggedIn)
+    {
+      dispatch(setUser(currentUser));
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <main className="h-100 min-h-screen bg-blue-300 font-poppins">
