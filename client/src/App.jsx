8b56e663d8d65f1533aca66c9ba5aa880af6d0b1
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-15 10:01:20 -0400

    Move browser router to index and reroute from home to /recipes if logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,32 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import RecipePage from "./pages/Recipes/RecipePage";
 import { setUser } from './state/actions/users'
 
 function App() {
   const dispatch = useDispatch();
-  
+  const navigate = useNavigate();
+
   // Checks if a user is saved on local storage and load their data
   useEffect(() => {
     let currentUser = JSON.parse(window.localStorage.getItem("RECIPE_USER"));
-  
+    
     if(currentUser?.isLoggedIn)
     {
       dispatch(setUser(currentUser));
+      navigate("/recipes");
     }
   }, [])
 
   return (
-    <BrowserRouter>
-      <main className="h-100 min-h-screen bg-blue-300 font-poppins">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="recipes" element={<RecipePage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className="h-100 min-h-screen bg-blue-300 font-poppins">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="recipes" element={<RecipePage />} />
+      </Routes>
+    </main>
   );
 }
 
