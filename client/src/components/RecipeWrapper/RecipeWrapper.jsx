25c3d839089ddfc5a4a4251068f3090cb6ebe479
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-16 11:28:34 -0400

    Improve error handling when user not logged in tries to access my recipes and liked recipes

--- client/src/components/RecipeWrapper/RecipeWrapper.jsx
+++ client/src/components/RecipeWrapper/RecipeWrapper.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Dropdown from 'react-dropdown';
 import { next, previous, setIndex } from "../../state/actions/recipeIndex";
-import { getAllRecipes, getMyRecipes, getMyLikedRecipes } from "../../state/actions/recipes";
+import { getAllRecipes, getMyRecipes, getMyLikedRecipes, clearRecipes } from "../../state/actions/recipes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +11,7 @@ import Recipe from "./Recipe/Recipe";
 
 const RecipeWrapper = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const recipeIndex = useSelector((state) => state.recipeIndex);
   const recipes = useSelector((state) => state.recipes);
@@ -26,38 +28,40 @@ const RecipeWrapper = () => {
   ];
   const defaultOption = options[0];
   const [currentOpt, setCurrentOpt] = useState(defaultOption);
+  const [errMsg, setErrMsg] = useState(undefined);
 
   useEffect(() => {
     switch(currentOpt.value) 
     {
-      case 1: 
+      case 1:
+        setErrMsg(undefined);
         dispatch(getAllRecipes());
         dispatch(setIndex(0));
         break;
 
       case 2:
-        if(username !== "")
+        if(username)
         {
           dispatch(getMyRecipes(username));
           dispatch(setIndex(0));
         }
         else
         {
-          alert("Login to view your recipes!");
-          setCurrentOpt(defaultOption);
+          setErrMsg("to view your recipes!");
+          dispatch(clearRecipes());
         }
         break;
 
       case 3:
-        if(username !== "")
+        if(username)
         {
           dispatch(getMyLikedRecipes(uid));
           dispatch(setIndex(0));
         }
         else
         {
-          alert("Login to view your liked recipes!");
-          setCurrentOpt(defaultOption);
+          setErrMsg("to view your liked recipes!");
+          dispatch(clearRecipes());
         }
         break;
 
@@ -102,7 +106,7 @@ const RecipeWrapper = () => {
       </section>
 
       <div className="flex justify-evenly text-center max-w-4xl mx-auto mt-10">
-        <Dropdown 
+        <Dropdown
           options={options} 
           onChange={e => setCurrentOpt(e)}
           value={currentOpt} 
@@ -111,11 +115,26 @@ const RecipeWrapper = () => {
           menuClassName="bg-blue-500 p-5 mt-1 rounded-lg shadow-lg shadow-slate-600"
         />
       </div>
-
-      {recipeCount === 0 ? (
-        <h2 className="font-bold text-center text-3xl mt-20">No Recipes</h2>
-      ) : (
-        <Recipe recipe={recipes[recipeIndex]} user={user} />
+      {errMsg ? (
+        <section className="flex justify-center font-bold text-center text-xl md:text-3xl lg:text-3xl sm:text-lg mt-20">
+          <button
+            className="font-bold mr-2 text-blue-800 hover:text-white focus:text-white"
+            onClick={() => navigate("/")}
+          >
+            Login
+          </button>
+          <h1 className="">
+            {errMsg}
+          </h1>
+        </section>
+      ):(    
+        <>
+          {recipeCount === 0 ? (
+            <h2 className="font-bold text-center text-3xl mt-20">No Recipes</h2>
+          ) : (
+            <Recipe recipe={recipes[recipeIndex]} user={user} />
+          )}
+        </>
       )}
     </>
   );
