28354becd0581f74526d88ac4575aa80c9fd586e
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-22 17:45:07 -0400

    Fix cors config

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/recipes", require("./routes/recipeRoutes"));
