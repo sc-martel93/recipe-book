af2335ed57f6a00eb4ac7e213f7cb4ad7490b5f5
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-22 12:42:20 -0400

    Add disable react dev tools dependency and disable

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+
+disableReactDevTools();
 
 ReactDOM.render(
   <React.StrictMode>
