b6a5d408d194558adc5fb114eb637ff7437c9752
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-06-22 23:07:34 -0400

    Disable react dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 
-// disableReactDevTools();
+disableReactDevTools();
 
 ReactDOM.render(
   <React.StrictMode>
