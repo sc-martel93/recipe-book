01478e474f1563fd86c74ced885cf0f11e4b5d1d
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-03-30 12:14:51 -0400

    Remove unused files

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,6 +1,5 @@
 import HomeHeader from "../../components/HomeHeader/HomeHeader";
 import Footer from "../../components/Footer/Footer";
-import ImageWrapper from "../../components/ImageWrapper/ImageWrapper";
 
 const HomePage = () => {
   return (
